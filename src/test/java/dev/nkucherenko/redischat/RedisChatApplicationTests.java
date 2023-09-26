8afc2a0a50277db72f510a9c168cbbca7151f76b
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2023-09-26 15:02:41 +0300

    initialize project

---
+++ src/test/java/dev/nkucherenko/redischat/RedisChatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nkucherenko.redischat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
