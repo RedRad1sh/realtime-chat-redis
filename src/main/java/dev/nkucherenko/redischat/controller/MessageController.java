0596005b9a5c93e1f64fa9e720cdd892ea821cf2
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2023-09-28 11:03:30 +0300

    Thymeleaf controller was disabled. Added Cors Settings

--- src/main/java/dev/nkucherenko/redischat/controller/MessageController.java
+++ src/main/java/dev/nkucherenko/redischat/controller/MessageController.java
@@ -2,7 +2,6 @@ package dev.nkucherenko.redischat.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.nkucherenko.redischat.component.MessageReceiver;
 import dev.nkucherenko.redischat.dto.Message;
 import dev.nkucherenko.redischat.service.MessageService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.ReactiveSubscription;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,18 +32,22 @@ public class MessageController {
 
     @PostMapping(value = "/post")
     public void postMessage(@RequestBody Message message) {
+        log.info(message.toString());
         message.setId(UUID.randomUUID());
         messageService.postMessage(message);
     }
 
     @GetMapping(value = "/chatAlive", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> subscribe() {
-        final Sinks.Many<String> sink = Sinks.many().multicast().directBestEffort();
-        Flux<String> stringFlux = Flux.just(messageService.getAllMessages()
-                .stream().map(Message::toString)
-                .collect(Collectors.toList())
-                .toArray(new String[]{}));
+    public Flux<Message> subscribe() {
+        Flux<Message> stringFlux = Flux.just(new ArrayList<>(messageService.getAllMessages())
+                .toArray(new Message[]{}));
 
-        return stringFlux.concatWith(container.receive(channelTopic).map(ReactiveSubscription.Message::getMessage));
+        return stringFlux.concatWith(container.receive(channelTopic).map(a -> {
+            try {
+                return objectMapper.readValue(a.getMessage(), Message.class);
+            } catch (JsonProcessingException jsonProcessingException) {
+                return new Message();
+            }
+        }));
     }
 }
--- src/main/java/dev/nkucherenko/redischat/controller/ThymeleafController.java
+++ src/main/java/dev/nkucherenko/redischat/controller/ThymeleafController.java
@@ -1,16 +1,21 @@
 package dev.nkucherenko.redischat.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.result.view.Rendering;
-import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
 
+/**
+ * Thymeleaf doesn't work properly with reactive streams. Page is loading forever
+ */
+@ConditionalOnExpression("${mono.thymeleaf.enabled:false}")
 @Controller
-public class FreemarkerController {
+@Slf4j
+public class ThymeleafController {
     @GetMapping("/chat")
     public String home(Model model) {
         WebClient client = WebClient.create("http://localhost:9092");
