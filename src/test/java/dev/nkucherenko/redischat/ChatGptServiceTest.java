85bfc0231778a02fee37faa7c88ae2c975af6899
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2023-10-12 16:02:31 +0300

    disable tests

--- src/test/java/dev/nkucherenko/redischat/ChatGptServiceTest.java
+++ src/test/java/dev/nkucherenko/redischat/ChatGptServiceTest.java
@@ -1,21 +1,22 @@
-package dev.nkucherenko.redischat;
-
-
-import dev.nkucherenko.redischat.service.ChatGptService;
-import dev.nkucherenko.redischat.service.impl.ChatGptServiceImpl;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.IOException;
-
-@SpringBootTest
-class ChatGptServiceTest {
-    ChatGptService chatGptService = new ChatGptServiceImpl();
-
-    @Test
-    @Disabled
-    void testRequest() throws IOException {
-        System.out.println(chatGptService.sendAndReceiveMessage("Кот", "Привет, как дела?"));
-    }
-}
+//package dev.nkucherenko.redischat;
+//
+//import dev.nkucherenko.redischat.service.ChatGptService;
+//import dev.nkucherenko.redischat.service.impl.ChatGptServiceImpl;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//import java.io.IOException;
+//
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class ChatGptServiceTest {
+//    ChatGptService chatGptService = new ChatGptServiceImpl();
+//
+//    @Test
+//    @Disabled
+//    void testRequest() throws IOException {
+//        System.out.println(chatGptService.sendAndReceiveMessage("Кот", "Привет, как дела?"));
+//    }
+//}
