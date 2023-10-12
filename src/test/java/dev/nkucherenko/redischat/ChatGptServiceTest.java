8052dbbae46ff5163107d2c83a25fb3973ad4cb4
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2023-10-12 15:55:38 +0300

    disable tests

--- src/test/java/dev/nkucherenko/redischat/ChatGptServiceTest.java
+++ src/test/java/dev/nkucherenko/redischat/ChatGptServiceTest.java
@@ -3,6 +3,7 @@ package dev.nkucherenko.redischat;
 
 import dev.nkucherenko.redischat.service.ChatGptService;
 import dev.nkucherenko.redischat.service.impl.ChatGptServiceImpl;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,6 +14,7 @@ class ChatGptServiceTest {
     ChatGptService chatGptService = new ChatGptServiceImpl();
 
     @Test
+    @Disabled
     void testRequest() throws IOException {
         System.out.println(chatGptService.sendAndReceiveMessage("Кот", "Привет, как дела?"));
     }
